0e9251e20d46e7acfa358938459e4a2db980f40f
Thomas Strub <thomstrub@gmail.com>
2021-03-11 13:11:30 -0800

    updates index.tsx in source directory

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Hello from './componenets/Hello'
+import Hello from './containers/Hello'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { enthusiasm } from './reducers/index';
+import { StoreState } from './types/index';
+
+const store = createStore<StoreState>(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: 'TypeScript',
+})
 
 ReactDOM.render(
-  <Hello name="TypeScript" enthusiasmLevel={10} />,
+  <Provider store={ store }>
+    <Hello />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
 
